95692e944805076d6f917e39401cb968d81262f4
timoleon <tilamprinos@gmail.com>
2023-06-11 19:55:36 +0300

    Fix test

--- src/test/kotlin/com/thechase/ApplicationTest.kt
+++ src/test/kotlin/com/thechase/ApplicationTest.kt
@@ -1,17 +1,24 @@
 package com.thechase
 
+import com.thechase.auth.JwtService
+import com.thechase.auth.hash
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
 import com.thechase.plugins.*
+import com.thechase.repository.QuestionRepository
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
+        val db = QuestionRepository()
+        val jwtService = JwtService()
+        val hashFunction = { s: String -> hash(s) }
+
         application {
-            configureRouting(db)
+            configureRouting(db, jwtService, hashFunction)
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
